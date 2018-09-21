c271921f45042a6815458fde24bba0925bc766b0
Đoàn Việt <vtahdn@gmail.com>
2018-09-21 16:27:28 +0700

    Custome

---
+++ NavController-TabbarController/BaseNavigationController.swift
@@ -0,0 +1,23 @@
+//
+//  BaseNavigationController.swift
+//  NavController-TabbarController
+//
+//  Created by Vinh The on 7/16/16.
+//  Copyright © 2016 Vinh The. All rights reserved.
+//
+
+import UIKit
+
+class BaseNavigationController: UINavigationController {
+    override func viewDidLoad() {
+        
+        // Make the arrow back white.
+        navigationBar.tintColor = UIColor.whiteColor()
+        
+        // Make the title white.
+        navigationBar.titleTextAttributes = [NSForegroundColorAttributeName : UIColor.whiteColor(), NSFontAttributeName : UIFont(name: "SFUIText-Regular", size: 22)!]
+        
+    }
+    
+}
+
